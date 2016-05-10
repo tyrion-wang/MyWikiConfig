20776410c81171fdc78119b51f3d9bce7e6a7763
Tyrion <maple_leaf_7@msn.com>
2016-05-10 17:15:33 +0800

    本地搜索图片

--- LocalSettings.php
+++ LocalSettings.php
@@ -77,7 +77,7 @@ $wgUseImageMagick = true;
 $wgImageMagickConvertCommand = "/usr/bin/convert";
 
 # InstantCommons allows wiki to use images from https://commons.wikimedia.org
-$wgUseInstantCommons = true;
+$wgUseInstantCommons = false;
 
 ## If you use ImageMagick (or any other shell command) on a
 ## Linux server, this will need to be set to the name of an
